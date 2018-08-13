8552ab0383206fd8ff88b9b25e1405e7911e45a7
DarkMoonCBK <darkmoon.cbk@me.com>
2018-08-13 23:01:21 +0800

    ..............

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -215,7 +215,7 @@ func (u *BlockUnlocker) handleBlock(block *rpc.GetBlockReply, candidate *storage
 		return err
 	}
 	candidate.Height = correctHeight
-	reward := getConstReward(candidate.Height)
+
 
 	// Add TX fees
 	extraTxReward, err := u.getExtraRewardForTx(block)
